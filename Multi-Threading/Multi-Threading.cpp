eee02488b5c9fb08eb3011100ab3eb6d8d4e3dd9
Kai Shen <shenkai029@gmail.com>
2022-02-21 17:01:03 -0800

    * Add use of std::shared_future

--- Multi-Threading/Multi-Threading.cpp
+++ Multi-Threading/Multi-Threading.cpp
@@ -47,6 +47,15 @@ void mythreadFutr(std::future<int>& futr) { // take future obj ref and print
     std::cout << "mythredFutr() end, " << "threadid = " << std::this_thread::get_id() << std::endl;
 }
 
+void mythreadSharedFutr(std::shared_future<int>& futr) { // take future obj ref and print
+    std::cout << "mythredFutr() start, " << "threadid = " << std::this_thread::get_id() << std::endl;
+    // std::future::get() was design use move semantic, so it cannot be use twice for same result since it will be empty
+    std::cout << "futr value: " << futr.get() << ", threadid = " << std::this_thread::get_id() << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::cout << "mythredFutr() end, " << "threadid = " << std::this_thread::get_id() << std::endl;
+}
+
+// std::shared_future is a class template. 
 
 
 int main()
@@ -57,10 +66,13 @@ int main()
     th_obj1.join(); // since we use std::thread here to create new thread, have to use join() here to wait
     
     std::future<int> result = th_pack.get_future();
+    std::shared_future<int> res_shared(std::move(result)); // or use result.share(), result will be empty after move
     //std::cout << "mythread() return value: " << result.get() << std::endl; // print thread result using get()
     //std::cout << "mythread() return value: " << result.get() << std::endl; // print thread result using get()
-    std::thread th_obj2(mythreadFutr, std::ref(result)); // pass future result to another thread
+    std::thread th_obj2(mythreadSharedFutr, std::ref(res_shared)); // pass future result to another thread
     th_obj2.join();
+    std::thread th_obj3(mythreadSharedFutr, std::ref(res_shared)); // pass future result to another thread
+    th_obj3.join();
     std::cout << "Hello World!" << std::endl;
 
     return 0;
